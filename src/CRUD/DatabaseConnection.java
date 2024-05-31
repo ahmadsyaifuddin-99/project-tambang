07b8b374805d419f2ee99382d4033a872a97c44b
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2024-05-31 15:24:32 +0800

    perubahan sedikit pada code Connection

--- src/CRUD/DatabaseConnection.java
+++ src/CRUD/DatabaseConnection.java
@@ -4,13 +4,10 @@
  */
 package CRUD;
 
-/**
- *
- * @author User
- */
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/2210010111_tambang";
@@ -19,5 +16,18 @@ public class DatabaseConnection {
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void main(String[] args) {
+        try (Connection connection = getConnection()) {
+            if (connection != null) {
+                JOptionPane.showMessageDialog(null, "Koneksi ke database berhasil!", "Success", JOptionPane.INFORMATION_MESSAGE);
+                System.out.println("Koneksi ke database berhasil!");
+
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Koneksi ke database gagal: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            System.err.println("Koneksi ke database gagal: " + e.getMessage());
+        }
     }
 }
